75f13231b73bc0a3b1b5ccc9e2757103468266a2
caicindy87 <caicindy87@gmail.com>
2020-08-17 17:35:09 -0700

    Add menu toggler for hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,45 +13,56 @@ class NavBar extends Component {
 
     return (
       <nav className="navbar">
-        <a href="/" className="logo">
-          <img src={logo} alt="logo" className="logo-img"></img>
-          Stay Current
-        </a>
-        <div className="navbar-right">
-          <Link to="/news">
-            <img src={newsIcon} alt="news-icon" className="icon"></img>
-            News
-          </Link>
-          {!!localStorage.getItem("token") ? (
-            <>
-              <Link to="/profile">
-                <img
-                  src={profileIcon}
-                  alt="profile-icon"
-                  className="icon"
-                ></img>
-                {currentUser.username}
-              </Link>
-            </>
-          ) : null}
-          {!!localStorage.getItem("token") ? (
-            <>
-              <Link
-                to="/"
-                onClick={() => {
-                  handleLogOut();
-                }}
-              >
-                <img src={logoutIcon} alt="logout-icon" className="icon"></img>
-                Log Out
-              </Link>
-            </>
-          ) : (
-            <Link to="/login">
-              <img src={loginIcon} alt="login-icon" className="icon"></img>
-              Log In
+        <div className="inner-width">
+          <a href="/" className="logo">
+            <img src={logo} alt="logo" className="logo-img"></img>
+            Stay Current
+          </a>
+          <button className="menu-toggler">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <div className="navbar-menu">
+            <Link to="/news">
+              <img src={newsIcon} alt="news-icon" className="icon"></img>
+              News
             </Link>
-          )}
+            {!!localStorage.getItem("token") ? (
+              <>
+                <Link to="/profile">
+                  <img
+                    src={profileIcon}
+                    alt="profile-icon"
+                    className="icon"
+                  ></img>
+                  {currentUser.username}
+                </Link>
+              </>
+            ) : null}
+            {!!localStorage.getItem("token") ? (
+              <>
+                <Link
+                  to="/"
+                  onClick={() => {
+                    handleLogOut();
+                  }}
+                >
+                  <img
+                    src={logoutIcon}
+                    alt="logout-icon"
+                    className="icon"
+                  ></img>
+                  Log Out
+                </Link>
+              </>
+            ) : (
+              <Link to="/login">
+                <img src={loginIcon} alt="login-icon" className="icon"></img>
+                Log In
+              </Link>
+            )}
+          </div>
         </div>
       </nav>
     );

22d5d8b1ae1d3c1b0b7471e86d26b3985f0730a0
caicindy87 <caicindy87@gmail.com>
2020-08-30 20:24:44 -0700

    Add methods to handle image upload and input change

--- src/components/myPosts/EditProfileForm.js
+++ src/components/myPosts/EditProfileForm.js
@@ -4,10 +4,40 @@ import { Form } from "semantic-ui-react";
 import "../../style/EditProfileForm.scss";
 
 class EditProfileForm extends Component {
-  state = {};
+  state = {
+    fields: {
+      text: "",
+      image: null,
+    },
+  };
+
+  componentDidMount() {
+    const { currentUser } = this.props;
+
+    this.setState({
+      fields: {
+        text: currentUser.bio,
+        image: currentUser.profile_pic,
+      },
+    });
+  }
+
+  handleInputChange = (e) => {
+    const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
+
+    this.setState({
+      fields: newFields,
+    });
+  };
+
+  onImageChange = (e) => {
+    const image = e.target.files[0];
+    this.setState({ fields: { ...this.state.fields, image: image } });
+  };
 
   render() {
     const { currentUser } = this.props;
+    const { fields } = this.state;
 
     return (
       <Form>
@@ -24,10 +54,10 @@ class EditProfileForm extends Component {
         </Form.Group>
         <Form.TextArea
           className="bio"
-          type="text"
+          type="textarea"
           name="text"
           label="Bio"
-          value={currentUser.bio}
+          value={fields.text}
           onChange={this.handleInputChange}
         ></Form.TextArea>
         <Form.Button className="save-edit-post-btn">Save changes</Form.Button>
